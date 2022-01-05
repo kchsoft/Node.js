e64e474f259ee8361deec7b9c0837cd4f4662d7a
changhyeon3 <53136613+changhyeon3@users.noreply.github.com>
2022-01-05 11:35:29 +0900

    practice about 'Not found'
    using pathname, i set error mesage 'Not found'

--- main.js
+++ main.js
@@ -2,44 +2,49 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 
+
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url,true).query; 
+    var pathname = url.parse(_url,true).pathname;
     var title = queryData.id;
-    console.log(queryData.id);
-    if(_url == '/'){
-      title = 'Welcome';
-    }
-    if(_url == '/favicon.ico'){
-        response.writeHead(404);
-        response.end();
-        return;
-    }
-    response.writeHead(200);
 
+    console.log(url.parse(_url,true).pathname);
+ 
+
+  if(pathname === '/') // pathname이 루트라면, path가 없는 경로로 왔다면
+  {
+    
     fs.readFile(`data/${queryData.id}`,'utf8',function(err,description){
+      //``는 템플릿, ${}는(QueryString) 템플릿 안에서 변수처럼 사용할 수 있게 해준다.
+     var template = `<!doctype html>
+     <html>
+     <head>
+       <title>WEB1 - ${title}</title>
+       <meta charset="utf-8">
+     </head>
+     <body>
+       <h1><a href="/">WEB</a></h1>
+       <ol>
+         <li><a href="/?id=HTML">HTML</a></li>
+         <li><a href="/?id=CSS">CSS</a></li>
+         <li><a href="/?id=JavaScript">JavaScript</a></li>
+       </ol>
+       <h2>${title}</h2>
+       <p>${description}</p>
+     </body>
+     </html>
+     `;
+     response.writeHead(200);
+     response.end(template); // template를 전송한다.
+   });
 
-       //``는 템플릿, ${}는 템플릿 안에서 변수처럼 사용할 수 있게 해준다.
-      var template = `<!doctype html>
-      <html>
-      <head>
-        <title>WEB1 - ${title}</title>
-        <meta charset="utf-8">
-      </head>
-      <body>
-        <h1><a href="/">WEB</a></h1>
-        <ol>
-          <li><a href="/?id=HTML">HTML</a></li>
-          <li><a href="/?id=CSS">CSS</a></li>
-          <li><a href="/?id=JavaScript">JavaScript</a></li>
-        </ol>
-        <h2>${title}</h2>
-        <p>${description}</p>
-      </body>
-      </html>
-      `;
-      response.end(template); // template를 전송한다.
-    })   
+  }
+  else{
+    response.writeHead(404); // 파일을 찾을수 없는 약속된 404
+    response.end('Not found');
+  }
+   
  
 });
 app.listen(3000); // 서버의 포트를 3000으로 맞춰준다.
