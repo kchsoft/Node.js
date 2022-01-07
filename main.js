0631156c56dadea541d8d5e792bcbf2e032c9ea4
changhyeon3 <53136613+changhyeon3@users.noreply.github.com>
2022-01-07 12:22:45 +0900

    practice about post method
    using
    <form action= method=> +78
    & request.on( )  +97,100
    & fs.writeFile( ) +104
    &  302,Location +105
    
    i can
    receive Information entered by the user
    & Add New File in dir
    & Show added web page by user

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+var qs = require('querystring');
 
 function templateHTML(title,list,body){
 
@@ -14,6 +15,7 @@ function templateHTML(title,list,body){
       <body>
         <h1><a href="/">WEB</a></h1>
         ${list}
+        <a href="/create">create</a>
         ${body}
       </body>
       </html>`;
@@ -64,7 +66,49 @@ var app = http.createServer(function(request,response){
             });
           });
         }
-  }
+  } 
+  else if( pathname === '/create'){
+        fs.readdir('./data',function(error,filelist){
+          //``는 템플릿, ${}는(QueryString) 템플릿 안에서 변수처럼 사용할 수 있게 해준다.
+        var title = "Web - create";
+        var description = "Hello, Node.js";
+        var list = templateList(filelist);
+        var template = templateHTML(title,list,`
+
+        <form action="http://localhost:3000/create_process" method="post">
+        <p>
+          <input type="text" name="title" placeholder="title">
+        </p>
+        <p>
+          <textarea name="description" placeholder="description"></textarea>
+        </p>
+        <p>
+          <input type="submit">
+        </p>
+         </form>
+        `); 
+
+        response.writeHead(200);
+        response.end(template); // template를 전송한다.
+      });
+    }
+    else if(pathname === '/create_process'){
+      var body ='';
+      request.on('data',function(data){
+        body = body + data;
+      });
+      request.on('end',function(){
+        var post = qs.parse(body);
+        var title = post.title;
+        var description = post.description;
+        fs.writeFile(`data/${title}`,description,'utf8',function(err){
+          response.writeHead(302,{Location:`/?id=${title}`}); // 302 -> page 다른 곳으로 이동시킴.(redirection)
+          response.end(); // template를 전송한다.
+        });
+      });
+      
+      
+    }
   else{
     response.writeHead(404); // 파일을 찾을수 없는 약속된 404
     response.end('Not found');
